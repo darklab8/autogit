feb209263cb79a27c7eee017e54d46ba02e31249
dd84ai <dd84ai@gmail.com>
2022-12-11 11:43:08 +0100

    refactor: moved cmds from parsing path to taskfile

--- changelog/main.go
+++ changelog/main.go
@@ -1,8 +1,5 @@
 package changelog
 
-// git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-
-// git log --pretty=format:'%s' --abbrev-commit
 import (
 	"autogit/git"
 )
--- semver/main.go
+++ semver/main.go
@@ -1,5 +1,1 @@
 package semver
-
-// git tag // list tags
-// git fetch --all --tags // fetching remote tags
-// git describe --tags // latest tag
