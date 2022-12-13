4f84679e7b4367ad5ca63e82593845925e67aaf5
dd84ai <dd84ai@gmail.com>
2022-12-13 04:39:31 +0100

    fix: about double vv

--- actions/about.go
+++ actions/about.go
@@ -10,6 +10,6 @@ func About() string {
 	var sb strings.Builder
 	sb.WriteString("OK ")
 	sb.WriteString("autogit version: ")
-	sb.WriteString(fmt.Sprintf("v%s", settings.Version))
+	sb.WriteString(fmt.Sprintf("%s", settings.Version))
 	return sb.String()
 }
