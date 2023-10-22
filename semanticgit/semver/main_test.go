a0183d4fa225db654269a5d0ed89e7d8cee13ae6
dd84ai <dd84ai@gmail.com>
2023-10-23 01:47:02 +0200

    feat: parse semvers without patch

--- semanticgit/semver/main_test.go
+++ semanticgit/semver/main_test.go
@@ -4,6 +4,8 @@ import (
 	"autogit/settings/testutils"
 	_ "autogit/settings/testutils/autouse"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSemver(t *testing.T) {
@@ -33,6 +35,21 @@ func TestSemver4(t *testing.T) {
 	testutils.Equal(t, 1, ver1.Major)
 	testutils.Equal(t, 0, ver1.Minor)
 	testutils.Equal(t, 1, ver1.Patch)
+	testutils.Equal(t, "a.1-b.2", ver1.Prerelease)
+	testutils.Equal(t, "324234", ver1.Build)
+}
+
+func TestSemverParseWithoutPatch(t *testing.T) {
+	ver1, err := Parse("v0.3")
+	assert.Nil(t, err, "returned error")
+	testutils.Equal(t, 0, ver1.Major)
+	testutils.Equal(t, 3, ver1.Minor)
+	testutils.Equal(t, 0, ver1.Patch)
+
+	ver1, _ = Parse("v2.0-a.1-b.2+324234")
+	testutils.Equal(t, 2, ver1.Major)
+	testutils.Equal(t, 0, ver1.Minor)
+	testutils.Equal(t, 0, ver1.Patch)
 	testutils.Equal(t, "a.1-b.2", ver1.Prerelease)
 	testutils.Equal(t, "324234", ver1.Build)
 }
