d61dc8ff6a76ce7a26b998fd4d73dde2a754fcd9
dd84ai <dd84ai@gmail.com>
2024-12-16 13:40:33 +0100

    feat: set reminder that autogit works

--- settings/logus/logger.go
+++ settings/logus/logger.go
@@ -3,5 +3,5 @@ package logus
 import "github.com/darklab8/go-typelog/typelog"
 
 var (
-	Log *typelog.Logger = typelog.NewLogger("autogit")
+	Log *typelog.Logger = typelog.NewLogger("autogit", typelog.WithLogLevel(typelog.LEVEL_INFO))
 )
